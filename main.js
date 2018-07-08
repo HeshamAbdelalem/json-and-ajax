81bb2ed46ccdd18fa863c9df3f80a625bd2069af
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2018-07-09 01:44:09 +0200

    feat: Finish the course and the code

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 /* jshint esversion:6 */
-let ourRequest = new XMLHttpRequest();
-
-ourRequest.open('GET', 'animals.json');
-ourRequest.onload = function() {
-    let ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-};
-ourRequest.send();
-
+let pageCounter = 1;
+let animalContainer = document.querySelector('#animal-info');
 let btn = document.querySelector('#btn');
-console.log(btn);
+
+btn.addEventListener('click', function(){
+
+    let ourRequest = new XMLHttpRequest();
+    /*
+    https://learnwebcode.github.io/json-example/animals-1.json */
+    ourRequest.open('GET', `animals-${pageCounter}.json`);
+    ourRequest.onload = function() {
+        let ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.onerror = function() {
+        console.log('Error!!');
+    }
+    ourRequest.send();
+    pageCounter++ ;
+    if(pageCounter > 3) {
+        btn.style.display ='none';
+    }
+
+});
+
+function renderHTML(data) {
+    let htmlString = '';
+
+    for(let i = 0; i < data.length; i++){
+        htmlString += `<p> ${data[i].name} is a ${data[i].species} that like to eat: ${data[i].foods.likes} </p>`;
+    }
+
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
